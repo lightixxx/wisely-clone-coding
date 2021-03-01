f0e125deb72b789bc960835ccfdb98d57afe7c73
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-03-01 21:12:36 +0900

    기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // eslint-disable-next-line
 
+// import './grid.min.css'
 import './reset.css'
-import './grid.min.css'
 import './App.css'
 import Header from './components/Header'
+import Landing from './components/Landing'
+import BrandStory from './components/BrandStory'
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Landing />
+      <BrandStory />
+
+      {/* <div></div> */}
+      <button type="button" className="chatBot"></button>
     </div>
   )
 }
