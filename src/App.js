c063e12d9fbedfecf92ec43a45b0aefff8532179
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-03-04 20:23:32 +0900

    add: Subscription, StartTrial component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // eslint-disable-next-line
 
-// import './grid.min.css'
+import './grid.min.css'
 import './reset.css'
 import './App.css'
 import Header from './components/Header'
 import Landing from './components/Landing'
 import BrandStory from './components/BrandStory'
+import Blade from './components/Blade'
+import Subscription from './components/Subscription'
+import StartTrial from './components/StartTrial'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -14,7 +18,10 @@ function App() {
       <Header />
       <Landing />
       <BrandStory />
-
+      <Blade />
+      <Subscription />
+      <StartTrial />
+      <Footer />
       {/* <div></div> */}
       <button type="button" className="chatBot"></button>
     </div>
